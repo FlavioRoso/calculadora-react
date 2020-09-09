c672d030d6880e7366c7e49b0c69cf59313c2f1a
FlavioRoso <flavio.roso@gmail.com>
2020-09-09 15:23:28 -0300

    Preparando para webhook

---
+++ build/precache-manifest.5c86d05afc8850b9a698a6f626bdaa16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd8725e5744958211db791ce82e727a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baaf4c88ce94b88e69b2",
+    "url": "/static/css/main.72513202.chunk.css"
+  },
+  {
+    "revision": "bdbf4a82a8595bc30bb3",
+    "url": "/static/js/2.ccb36bf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ccb36bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baaf4c88ce94b88e69b2",
+    "url": "/static/js/main.b0c99faf.chunk.js"
+  },
+  {
+    "revision": "e54719769ab0f2288bbb",
+    "url": "/static/js/runtime-main.5757a36a.js"
+  }
+]);
